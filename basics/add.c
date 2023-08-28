72e5b5877ca2558d7018bd81950bdc4993bf6f39
Anugya <reddyanugya@gamil.com>
2023-08-28 19:09:29 +0530

    add.c

---
+++ basics/add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int x,y;
+	printf("Enter 1st no. = ");
+	scanf("%d",&x);
+
+	printf("Enter 2nd No. =");
+	scanf("%d",&y);
+	
+	int sum=(x+y);
+	printf("Sum is %d",sum);
+return 0;
+}
